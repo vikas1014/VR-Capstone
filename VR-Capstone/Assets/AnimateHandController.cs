09867539de9d236aae9b70cef4de0267c49a0d1e
vikas1014 <manvikas1234@gmail.com>
2024-12-08 23:31:38 -0600

    Update AnimateHandController.cs description

--- VR-Capstone/Assets/AnimateHandController.cs
+++ VR-Capstone/Assets/AnimateHandController.cs
@@ -1,39 +1,26 @@
-// Found in video 2 of 2023 unity VR basics
+// Found in video 2 of 2023 Unity VR basics
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Controls hand animations in VR by reading input from grip and trigger actions.
+/// Requires an Animator component to animate the hand model.
+/// </summary>
 [RequireComponent(typeof(Animator))]
 public class AnimateHandController : MonoBehaviour
 {
+    // References to Input Actions for grip and trigger
     public InputActionReference gripInputActionReference;
     public InputActionReference triggerInputActionReference;
 
+    // Animator component to control the hand animations
     private Animator _handAnimator;
+
+    // Values for grip and trigger inputs
     private float _gripValue;
     private float _triggerValue;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _handAnimator = GetComponent<Animator>();
-    }
+    /// <summary>
+    /// Called once when the script is initialized.
+    /// Sets up the
 
-    // Update is called once per frame
-    void Update()
-    {
-        AnimateGrip();
-        AnimateTrigger();
-    }
-
-    private void AnimateGrip()
-    {
-        _gripValue = gripInputActionReference.action.ReadValue<float>();
-        _handAnimator.SetFloat("Grip", _gripValue);
-    }
-
-    private void AnimateTrigger()
-    {
-        _triggerValue = triggerInputActionReference.action.ReadValue<float>();
-        _handAnimator.SetFloat("Trigger", _triggerValue);
-    }
-}
